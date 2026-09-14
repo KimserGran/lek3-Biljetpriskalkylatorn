eb67a196e02a727ed042149ffd4dbfab0c77e163
kim granzell <kimzell_94@hotmail.se>
2026-09-14 10:41:43 +0200

    Add project files.

---
+++ 02 Ålderskontrollen/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02_Ålderskontrollen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange din ålder:");
+            int ålder = int.TryParse(Console.ReadLine(), out var parsedValue) ? parsedValue : 0;
+            bool ärMyndig = ålder >= 18;
+            if(ärMyndig)
+            {
+                Console.WriteLine("Du är myndig.");
+            }
+            else
+            {
+                Console.WriteLine("Du är inte myndig.");
+            }
+        }
+    }
+}
---
+++ lek3 Biljetpriskalkylatorn/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Channels;
+
+namespace lek3_Biljetpriskalkylatorn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal grundPris = 120;
+            Console.WriteLine("Hur många biljetter vill du köpa?");
+            int köptaBiljetter = int.TryParse(Console.ReadLine(), out var parsedValue) ? parsedValue : 0;
+            decimal totalPris = grundPris * köptaBiljetter;
+            decimal rabatt = 0.9m;
+            if (köptaBiljetter >= 3)
+            {
+                Console.WriteLine($"Totala priset är: {totalPris * rabatt} kr (10% rabatt)");
+            }
+            else
+            {
+                Console.WriteLine($"Totala priset är: {totalPris} kr");
+            }
+        }
+    }
+}
